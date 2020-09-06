c75e90f029a0109407a0d73dcc65f5abb04ee06a
Eugene St <Eugene.stepchenko@gmail.com>
2020-09-06 09:23:45 +0300

    Initial Commit

---
+++ CoctailBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoctailBar
+//
+//  Created by Eugene St on 06.09.2020.
+//  Copyright © 2020 Eugene St. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
