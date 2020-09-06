4791b592cb80f6de18acc9beb4bc258ffe1bdbaa
Eugene St <Eugene.stepchenko@gmail.com>
2020-09-06 09:35:49 +0300

    Structured Project Navigation

---
+++ CoctailBar/Protocols/DataProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  DataProtocol.swift
+//  CoctailBar
+//
+//  Created by Eugene St on 06.09.2020.
+//  Copyright © 2020 Eugene St. All rights reserved.
+//
+
+import Foundation
---
+++ CoctailBar/Protocols/ListProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ListProtocol.swift
+//  CoctailBar
+//
+//  Created by Eugene St on 06.09.2020.
+//  Copyright © 2020 Eugene St. All rights reserved.
+//
+
+import Foundation
